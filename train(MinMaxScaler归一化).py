8451bab4bbd3ebd6666b24c561e9e79405ea1804
102757017 <sdf63fg@yeah.net>
2020-08-06 20:22:43 +0800

    keras替换为tensorFlow.keras
    Signed-off-by: 102757017 <sdf63fg@yeah.net>

--- train(MinMaxScaler归一化).py
+++ train(MinMaxScaler归一化).py
@@ -1,15 +1,24 @@
 import datetime
 import os
+import sys
 import time
 import warnings
 import numpy as np
 from numpy import newaxis
-
+'''
 from keras.layers.core import Dense, Activation, Dropout
 from keras.layers.recurrent import LSTM
 from keras.models import Sequential
+from keras.callbacks import TensorBoard
+'''
+from tensorflow.keras.layers import Dense, Activation, Dropout,LSTM
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.callbacks import TensorBoard
 
 from sklearn.preprocessing import MinMaxScaler
+from selenium import webdriver
+
+
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' #Hide messy TensorFlow warnings
 warnings.filterwarnings("ignore") #Hide messy Numpy warnings
@@ -18,7 +27,7 @@ warnings.filterwarnings("ignore") #Hide messy Numpy warnings
 
 
 #seq_len为周期数，即每次使用多少天的数据来推测结果
-def load_data2(filename, seq_len,batch_size):
+def load_data2(filename, seq_len):
     #将csv文件载入为numpy一维数组
     my_matrix = np.loadtxt(open(filename,"rb"),delimiter=",",skiprows=0)
 
@@ -33,7 +42,7 @@ def load_data2(filename, seq_len,batch_size):
         set1[index,:]=temp
 
     #训练集数量
-    n_train = round(0.9 * (my_matrix.size - seq_len)/batch_size)*batch_size
+    n_train = round(0.9 * (my_matrix.size - seq_len))
     
     #print(n_train,set1[1])
     #测试集数量
@@ -47,17 +56,17 @@ def load_data2(filename, seq_len,batch_size):
 
 
     
-    scaler = MinMaxScaler(feature_range=(0, 1)).fit(x_train)
+    scaler = MinMaxScaler(feature_range=(-1, 1)).fit(x_train)
     x_train = scaler.transform(x_train)
 
-    scaler = MinMaxScaler(feature_range=(0, 1)).fit(y_train)
+    scaler = MinMaxScaler(feature_range=(-1, 1)).fit(y_train)
     y_train = scaler.transform(y_train)
 
-    scaler = MinMaxScaler(feature_range=(0, 1)).fit(x_test)
+    scaler = MinMaxScaler(feature_range=(-1, 1)).fit(x_test)
     
     x_test = scaler.transform(x_test)
 
-    scaler = MinMaxScaler(feature_range=(0, 1)).fit(y_test)
+    scaler = MinMaxScaler(feature_range=(-1, 1)).fit(y_test)
     y_test = scaler.transform(y_test)
 
 
@@ -70,27 +79,23 @@ def load_data2(filename, seq_len,batch_size):
 
     
 
-def build_model(layers):
+def build_model(input_shape,midout,output3):
     model = Sequential()
     #inputshape=(50, 1): 当把这层作为某个模型的第一层时，需要用到该参数，输入矩阵形状：(n_samples, dim_input)，dim_input指影响输出的特征数，此例为单因子模型所以填1，当建立多因子模型时填因子个数
-    #stateful=True：将一个很长的序列（例如时间序列）分成小序列来构建我的输入矩阵，输入的batch的顺序很重要，并且各个batch的大小必须相等（包括最后一个batch），因此需要使用batch_input_shape代替inputshape指定batch_size的大小。
-    #stateful=Flase：输入batch的顺序可以打乱，不需要指定batch_size的大小，因此使用inputshape参数就可以了
-    #output_dim=50: 隐藏层神经元的个数，输出长度为50的向量，因为每个时间步都输出一次，共50个时间步，所以最后得到50*50的序列，至于输出整个序列还是最后一个序列取决于参数return_sequences
+    #units=50: 隐藏层神经元的个数，共50个，所以最后得到50*50的序列，至于输出整个序列还是最后一个序列取决于参数return_sequences
     #return_sequences：默认False，控制返回类型。若为True则返回整个序列，否则仅返回输出序列的最后一个输出
-    model.add(LSTM(units=layers[1],batch_input_shape=(256,layers[1], layers[0]),return_sequences=True, stateful=True))
+    model.add(LSTM(input_shape=input_shape, units=50, return_sequences=True))
     print('lstm层输出n个50*50的矩阵',model.output_shape)
-    model.add(Dropout(0.2))
+    model.add(Dense(units=50))
     print('Dropout输出',model.output_shape)
 
     #第一个参数指的是输出的维度，这和第一个LSTM的输出维度并不一样，这也是LSTM比较“随意”的地方
     #LSTM(100,return_sequences=False)
-    model.add(LSTM(layers[2],return_sequences=False, stateful=True))
+    model.add(LSTM(midout, return_sequences=False))
     print('第二个lstm层输出',model.output_shape)
-    model.add(Dropout(0.2))
-    print('Dropout输出',model.output_shape)
 
-    #output_dim=1
-    model.add(Dense(output_dim=layers[3]))
+    #units=1
+    model.add(Dense(units=output3))
     print('全连接层输出',model.output_shape)
 
     #激活函数可以通过设置单独的激活层实现，也可以在构造层对象时通过传递activation参数实现
@@ -133,9 +138,9 @@ def predict_sequences_multiple(model, data, window_size, prediction_len):
 
 
 
-batch_size = 256
 
-X_train, y_train, X_test, y_test = load_data2('sinwave.csv', 50,batch_size)
+
+X_train, y_train, X_test, y_test = load_data2('sp500差分.csv', 50)
 
 '''
 print(X_train.shape)
@@ -147,21 +152,36 @@ print("\n")
 print(X_train[1,:,:],y_train[1])
 '''
 
-model = build_model([1, 50, 100, 1])
-model.fit(X_train,y_train,batch_size=256,nb_epoch=1)
+
+model=build_model(input_shape=(50,1),midout=100,output3=1)
+model.fit(X_train,y_train,batch_size=256,epochs=3,validation_split=0.05,callbacks=[TensorBoard(log_dir='log')])
 print("\n")
+
+
+'''
+exe=os.path.join(sys.prefix,"Scripts","tensorboard.exe")
+obj=os.path.join(os.path.dirname(__file__),"log")
+cmd="\""+exe+"\""+" --logdir="+obj
+print("command命令是："+ cmd)
+
+browser = webdriver.Chrome()
+browser.get('localhost:6006')
+
+os.system(cmd) #执行command
+'''
+
 
 
 # 保存模型
 model.save('model.h5')   # HDF5文件，pip install h5py
 
-y_predict=model.predict(X_test[0:512],batch_size=256,verbose=1)
+y_predict=model.predict(X_test[0:100],batch_size=256,verbose=1)
 
 # 可视化
 import matplotlib.pyplot as plt
 
 
-plt.plot(y_test,label="true")
+plt.plot(y_test[0:100],label="true")
 plt.plot(y_predict,label="predict")
 
 #绘制标签栏
--- train(比值减1进行归一化).py
+++ train(比值减1进行归一化).py
@@ -4,12 +4,10 @@ import time
 import warnings
 import numpy as np
 from numpy import newaxis
+from tensorflow.keras.layers import Dense, Activation, Dropout,LSTM
 
-from keras.layers.core import Dense, Activation, Dropout
-from keras.layers.recurrent import LSTM
-from keras.models import Sequential
-
-from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras.models import Sequential
+import pandas as pd
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' #Hide messy TensorFlow warnings
 warnings.filterwarnings("ignore") #Hide messy Numpy warnings
@@ -18,7 +16,7 @@ warnings.filterwarnings("ignore") #Hide messy Numpy warnings
 
 
 #seq_len为周期数，即每次使用多少天的数据来推测结果
-def load_data2(filename, seq_len,batch_size):
+def load_data2(filename, seq_len):
     #将csv文件载入为numpy一维数组
     my_matrix = np.loadtxt(open(filename,"rb"),delimiter=",",skiprows=0)
 
@@ -29,11 +27,12 @@ def load_data2(filename, seq_len,batch_size):
     for index in range(my_matrix.size - seq_len):
         #输入数据是以行为单位进行训练的，需要将提取的矩阵转置为行向量
         temp=my_matrix[index:index+sequence_length,].T
-        
+        #归一化，将数组元素转换为-1~1之间的小数，目的是为了加快计算速度，归一化只能针对每行数据分别进行归一，不能对整体数组归一。
+        temp=temp/temp[0]-1
         set1[index,:]=temp
 
     #训练集数量
-    n_train = round(0.9 * (my_matrix.size - seq_len)/batch_size)*batch_size
+    n_train = round(0.9 * (my_matrix.size - seq_len))
     
     #print(n_train,set1[1])
     #测试集数量
@@ -45,27 +44,11 @@ def load_data2(filename, seq_len,batch_size):
     x_test=set1[n_train:,0:-1]
     y_test=set1[n_train:,-1]
 
-
-    
-    scaler = MinMaxScaler(feature_range=(0, 1)).fit(x_train)
-    x_train = scaler.transform(x_train)
-
-    scaler = MinMaxScaler(feature_range=(0, 1)).fit(y_train)
-    y_train = scaler.transform(y_train)
-
-    scaler = MinMaxScaler(feature_range=(0, 1)).fit(x_test)
-    
-    x_test = scaler.transform(x_test)
-
-    scaler = MinMaxScaler(feature_range=(0, 1)).fit(y_test)
-    y_test = scaler.transform(y_test)
-
-
-
     #此例为单因子模型，所以最后一个维度填1
     x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
     x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
-    print(x_train.shape,y_train.shape,x_test.shape,y_test.shape)   
+    print(x_train.shape,y_train.shape,x_test.shape,y_test.shape)
+
     return [x_train, y_train, x_test, y_test]
 
     
@@ -73,24 +56,22 @@ def load_data2(filename, seq_len,batch_size):
 def build_model(layers):
     model = Sequential()
     #inputshape=(50, 1): 当把这层作为某个模型的第一层时，需要用到该参数，输入矩阵形状：(n_samples, dim_input)，dim_input指影响输出的特征数，此例为单因子模型所以填1，当建立多因子模型时填因子个数
-    #stateful=True：将一个很长的序列（例如时间序列）分成小序列来构建我的输入矩阵，输入的batch的顺序很重要，并且各个batch的大小必须相等（包括最后一个batch），因此需要使用batch_input_shape代替inputshape指定batch_size的大小。
-    #stateful=Flase：输入batch的顺序可以打乱，不需要指定batch_size的大小，因此使用inputshape参数就可以了
-    #output_dim=50: 隐藏层神经元的个数，输出长度为50的向量，因为每个时间步都输出一次，共50个时间步，所以最后得到50*50的序列，至于输出整个序列还是最后一个序列取决于参数return_sequences
+    #units=50: 隐藏层神经元的个数,输出长度为50的向量，因为每个时间步都输出一次，共50个时间步，所以最后得到50*50的序列，至于输出整个序列还是最后一个序列取决于参数return_sequences
     #return_sequences：默认False，控制返回类型。若为True则返回整个序列，否则仅返回输出序列的最后一个输出
-    model.add(LSTM(units=layers[1],batch_input_shape=(256,layers[1], layers[0]),return_sequences=True, stateful=True))
+    model.add(LSTM(units=50,input_shape=(layers[1], layers[0]),return_sequences=True))
     print('lstm层输出n个50*50的矩阵',model.output_shape)
     model.add(Dropout(0.2))
     print('Dropout输出',model.output_shape)
 
     #第一个参数指的是输出的维度，这和第一个LSTM的输出维度并不一样，这也是LSTM比较“随意”的地方
     #LSTM(100,return_sequences=False)
-    model.add(LSTM(layers[2],return_sequences=False, stateful=True))
+    model.add(LSTM(layers[2],return_sequences=False))
     print('第二个lstm层输出',model.output_shape)
     model.add(Dropout(0.2))
     print('Dropout输出',model.output_shape)
 
-    #output_dim=1
-    model.add(Dense(output_dim=layers[3]))
+    #units=1
+    model.add(Dense(units=layers[3]))
     print('全连接层输出',model.output_shape)
 
     #激活函数可以通过设置单独的激活层实现，也可以在构造层对象时通过传递activation参数实现
@@ -133,9 +114,9 @@ def predict_sequences_multiple(model, data, window_size, prediction_len):
 
 
 
-batch_size = 256
 
-X_train, y_train, X_test, y_test = load_data2('sinwave.csv', 50,batch_size)
+
+X_train, y_train, X_test, y_test = load_data2('sp500.csv', 50)
 
 '''
 print(X_train.shape)
@@ -148,14 +129,14 @@ print(X_train[1,:,:],y_train[1])
 '''
 
 model = build_model([1, 50, 100, 1])
-model.fit(X_train,y_train,batch_size=256,nb_epoch=1)
+model.fit(X_train,y_train,batch_size=512,nb_epoch=1,validation_split=0.05)
 print("\n")
 
 
 # 保存模型
 model.save('model.h5')   # HDF5文件，pip install h5py
 
-y_predict=model.predict(X_test[0:512],batch_size=256,verbose=1)
+y_predict=model.predict(X_test,batch_size=512,verbose=1)
 
 # 可视化
 import matplotlib.pyplot as plt
